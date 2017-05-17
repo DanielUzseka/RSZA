e6a0f4a68c58c2c053a47afb8bdce96dadbe4cfb
Unknown <kristofkalocsai@gmail.com>
2017-05-17 23:00:57 +0200

    added specification
    and explained the unimplemented fns

--- project/macros.vh
+++ project/macros.vh
@@ -1,22 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    08:52:53 04/29/2017 
-// Design Name: 
-// Module Name:    macros 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 `ifndef macros_vh
 `define macros_vh
 
@@ -24,4 +5,3 @@
 `define dataWidth 32
 
 `endif
-
--- project/mod_top.v
+++ project/mod_top.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    07:42:55 04/07/2017
-// Design Name:
-// Module Name:    mod_top
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
 
 `include "macros.vh"
 
@@ -25,16 +6,16 @@
 
 module mod_top
 (
-	input			PCLK,      // The rising edge of PCLK is used to time all transfers on the APB.
-   input			PRESETn,   // The APB bus reset signal is active LOW and this signal will normally be connected directly to the system bus reset signal.
-	input 	[`addrWidth-1:0]	PADDR,     // This is the APB address bus, which may be up to 32-bits wide and is driven by the peripheral bus bridge unit.
-	input 			PSELx,     // A signal from the secondary decoder, within the peripheral bus bridge unit, to each peripheral bus slave x. This signal indicates that the slave device is selected and a data transfer is required. There is a PSELx signal for each bus slave.
-	input 			PENABLE,   // This strobe signal is used to time all accesses on the peripheral bus. The enable signal is used to indicate the second cycle of an APB transfer. The rising edge of PENABLE occurs in the middle of the APB transfer.
-	input 			PWRITE,    // When HIGH this signal indicates an APB write access and when LOW a read access.
-	output	[`dataWidth-1:0]	PRDATA,    // The read data bus is driven by the selected slave during read cycles (when PWRITE is LOW). The read data bus can be up to 32-bits wide.
-	input 	[`dataWidth-1:0] 	PWDATA,     // The write data bus is driven by the peripheral bus bridge unit during write cycles (when PWRITE is HIGH). The write data bus can be up to 32-bits wide.
-	inout SDA,
-	output SCL
+	input 					PCLK,
+	input 					PRESETn,
+	input 	[`addrWidth-1:0]		PADDR,
+	input 					PSELx,
+	input 					PENABLE,
+	input 					PWRITE,
+	output 	[`dataWidth-1:0]		PRDATA,
+	input  	[`dataWidth-1:0] 		PWDATA,
+	inout 					SDA,
+	output 					SCL
 );
 
 // apb_mod
@@ -46,32 +27,32 @@ module mod_top
 	wire it_enable;
 	wire [`dataWidth-1:0] per_addr; //azert datawith nem pedig addrwith mert ez az i2c periferiaira vonatkozik
 	wire [`dataWidth-1:0] per_data;
-	
+
 	wire [`dataWidth-1:0] A2I;
 	wire [`dataWidth-1:0] I2A;
-	
+
 // Instantiate the module
 apb_mod inst_APB (
-    .clk(PCLK), 
-    .reset(PRESETn), 
-    .paddr(PADDR), 
-    .pwdata(PWDATA), 
-    .prdata(PRDATA), 
-    .pwrite(PWRITE), 
-    .psel(PSELx), 
-    .penable(PENABLE), 
-    .in_perdata(I2A), 
+    .clk(PCLK),
+    .reset(PRESETn),
+    .paddr(PADDR),
+    .pwdata(PWDATA),
+    .prdata(PRDATA),
+    .pwrite(PWRITE),
+    .psel(PSELx),
+    .penable(PENABLE),
+    .in_perdata(I2A),
     .out_perdata(A2I)
     );
-	 
+
 // Instantiate the module
 mod_I2C inst_I2C (
-    .SDA(SDA), 
-    .SCL(SCL), 
+    .SDA(SDA),
+    .SCL(SCL),
     .dataIn(A2I),
-	 .dataOut(I2A),
-    .clk(PCLK), 
-    .rst(PRESETn) 
+    .dataOut(I2A),
+    .clk(PCLK),
+    .rst(PRESETn)
     );
-	 
+
 endmodule
